d2247e40fa2f2232e9853dc9b488792be4b7a948
alexandros-kourtidis <alex.kourtidis.tz@gmail.com>
2025-10-20 18:41:33 +0200

    Add files via upload

---
+++ 04-variant-calling/run_04_variantcalling_mt_batch1234CN.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -l
+ 
+#SBATCH --cluster=genius
+#SBATCH --job-name=genotyping
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=48:00:00
+#SBATCH -A lp_svbelleghem
+#SBATCH --array=1 # Make sure the array size matches your sample count (D. magna: 10 chromosomes, 10+ scaffolds)
+ 
+# Load the programs we will use
+module load SAMtools/1.16.1-GCC-11.3.0
+module load cluster/genius/amd
+module load BCFtools/1.15.1-GCC-11.3.0
+module load VCFtools
+module load HTSlib/1.17-GCC-12.2.0
+ 
+echo "================="
+ 
+cd /lustre1/scratch/363/vsc36396/alignments_all/
+IN_DIR=.
+OUT_DIR="VCFs"
+mkdir -p $OUT_DIR
+VCF="CN_DmagnaLRV01"
+
+REF=/lustre1/scratch/363/vsc36396/reference_LRV_01/DmagnaLRV01.fasta
+
+	#This variable will store the job array number minus 1, so we can use it to get a sample from the samples list (index starts at 0)
+ID=$((SLURM_ARRAY_TASK_ID -1))
+
+	# Chromosome names and samples IDs
+chrom=(Mt)
+samples=($(cat /vsc-hard-mounts/leuven-data/363/vsc36396/scripts/04_genotyping/batch1234CN_list_sequence.txt))
+ 
+	# make a single list of all the samples that can be used in the samtools command
+ALL_LIST=""
+for FILE in ${samples[@]}
+do
+        ALL_LIST="$ALL_LIST $IN_DIR/${FILE}_aligned.sorted.nd.bam"
+done
+ 
+	# Use eval to ensure the full list is constructed correctly
+eval command="\$ALL_LIST"
+
+	# run mpileup
+	# Non-variant sites are included.
+	# Will include both allelic and total depths (AD and DP), and the output format will be GT:GQ:GP (genotype: genotype quality: genotype posterior probability)
+bcftools mpileup -O z --threads 16 -f $REF -r ${chrom[$ID]} $(echo $command) -a AD,DP | \
+bcftools call -m -O z --threads 16 -f GQ,GP -o $OUT_DIR/${VCF}_${chrom[$ID]}.vcf.gz
+
+	# Index the VCF
+bcftools index $OUT_DIR/${VCF}_${chrom[$ID]}.vcf.gz
+
+	#Calculate missing data for each individual/sample and prints it to an out.imiss file
+vcftools --gzvcf $OUT_DIR/${VCF}_${chrom[$ID]}.vcf.gz --missing-indv --out $OUT_DIR/${VCF}_${chrom[$ID]}.vcf.gz
+ 
+echo "genotyping done"
