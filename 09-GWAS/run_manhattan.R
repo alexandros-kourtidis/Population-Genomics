187a44de4363f1ad225066658d2d5217db91b0d7
alexandros-kourtidis <alex.kourtidis.tz@gmail.com>
2026-01-23 11:37:52 +0100

    Update run_manhattan.R

--- 09-GWAS/run_manhattan.R
+++ 09-GWAS/run_manhattan.R
@@ -9,7 +9,7 @@ suppressPackageStartupMessages({
 
 # --- Parameters (edit as needed) ---
 phenotype <- "b12kris_A5B1_07ugL"
-gwas_file <- "gemma_results/phenotype_b12kris_A5B1_07ugL_gemma_lmm_results_nocov.assoc.txt"  # GEMMA output
+gwas_file <- paste0("gemma_results/phenotype_", phenotype, "_gemma_lmm_results_nocov.assoc.txt")  # GEMMA output
 output_png <- paste0("plots/manhattan_plot_", phenotype, ".png")                     # Output image
 scaffold_file <- "scaffold_lengths.txt"                           # Scaffold lengths from VCF header
 downsample <- 1e5                                            # number of SNPs to plot; set NULL to plot all
@@ -81,5 +81,6 @@ ggplot(gwas_plot, aes(x = pos_cum, y = -log10(p_wald))) +
     axis.title = element_text(size = 14)
   )
 dev.off()
+
 
 
