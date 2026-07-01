4b42683c85cf5db0d96ab1f14f659b24f763abc4
Alexandros Kourtidis <alex.kourtidis.tz@gmail.com>
2026-07-01 15:21:59 +0200

    Update 05_optm_analysis.R

--- 12-treemix/05_optm_analysis.R
+++ 12-treemix/05_optm_analysis.R
@@ -6,20 +6,33 @@ rm(list=ls())
 library(OptM)
 
 indir <- "treemix_mruns"
-outdir <- "OptM_results"
+outdir <- "OptM_results/"
 dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
-
-
-folder <- "treemix_mruns"
 
 # Default method
 test.optM <- optM(indir)
 plot_optM(test.optM, method = "Evanno")
 
+
 # Linear/change-point methods
 test.linear <- optM(indir, method = "linear")
 plot_optM(test.linear, method = "linear")
 
+png(filenam=paste0(outdir, "OptM_linear.png"), 
+    width = 2000,
+    height = 1700,
+    res = 300)
+plot_optM(test.linear, method = "linear")
+dev.off()
+
 # SiZer method
 test.sizer <- optM(indir, method = "SiZer")
 plot_optM(test.sizer, method = "SiZer")
+
+png(filenam=paste0(outdir, "OptM_SiZer.png"), 
+    width = 2000,
+    height = 1700,
+    res = 300)
+plot_optM(test.sizer, method = "SiZer")
+dev.off()
+
